b275a7343cadf3776920bc8892c096b43e4f9a92
scutLMG <1392327780@qq.com>
2021-07-11 16:32:43 +0800

    Site updated: 2021-07-11 16:32:36

---
+++ ckin/ckin.js
@@ -0,0 +1,10 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.css"><script src="https://cdn.jsdelivr.net/npm/aplayer/dist/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="https://cdn.jsdelivr.net/npm/meting/dist/Meting.min.js"></script>function pass(){
+    var x = prompt ("请输入访问密码: \n Enter your password:");/*第一个变量为提示语，第二个变量为默认初始值*/
+    if (x === "jianchijiushishengli"){
+        alert(my_date);
+    }
+    else{
+        alert("这是个人应用页面，只有站长能用哦。(施工中)\nThis is a private app for blogger, you can only check the calendar.");
+    }
+}
+
