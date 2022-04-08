2b0f64df0ca94122d85bfe36c1ea88a39f54afcc
scutLMG <1392327780@qq.com>
2022-04-08 17:48:16 +0800

    Site updated: 2022-04-08 17:48:02

--- js/main.js
+++ js/main.js
@@ -120,7 +120,6 @@
 
     const $toc = $('#toc');
     if ($toc.length > 0) {
-        // $toc.addClass('column-right is-sticky');
         const $mask = $('<div>');
         $mask.attr('id', 'toc-mask');
 
